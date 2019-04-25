88fbf2aea825329a9f1a43d4611ad8b022783297
Michael P <31676277+Perez178@users.noreply.github.com>
2019-04-24 21:31:44 -0700

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,228 @@
+/*
+Michael Perez
+CSC 337, Spring 2019
+Final Homework
+
+
+Java script for the pictures, and the get and send to the server
+used for Home.html, About.html, ExecutiveBoard.html, Events.html, Member.html
+*/
+
+
+
+//Desktop\337 Projects\Final Homework
+"use strict";
+(function() {
+
+	/** connects the buttons to the functions**/
+	/** divides the onload functions by what page its on**/
+	window.onload = function() {
+		let path = window.location.pathname;
+		let page = path.split("/").pop();
+
+		if(page === "Home.html"){
+			callAjax("Photos", "Group");
+		}else if(page === "About.html"){
+			callAjax("Photos", "About");
+		}else if(page === "ExecutiveBoard.html"){
+			callAjax("Members", "none");
+		}else if(page === "Events.html"){
+			callAjax("Photos", "Events");
+		}else if(page === "Member.html"){
+			callAjax("newMembers", "0");
+			setInterval(function(){callAjax("newMembers", "0"); }, 5000);
+			document.getElementById("submit").onclick = send;
+		}
+
+
+
+	};
+
+	/** gets the appropriate data from server depending on parameters **/
+		function callAjax(mode, name){
+			let url = "http://localhost:" +  process.env.PORT + "/?mode="+mode+"&name=" + name;
+			//let url = "http://eller" +  process.env.PORT + "/?mode="+mode+"&name=" + name + ".herokuapp.com"
+			fetch(url)
+				.then(checkStatus)
+				.then(function(responseText) {
+					if(mode === "Photos"){
+						slideShow(responseText, name);
+					}else if(mode === "newMembers"){
+						myNews(responseText);
+					}else{
+						myMembers(responseText);
+					}
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
+			}
+
+/** sets up the picture scroller based on what folder it reads from**/
+			function slideShow(responseText, name){
+				let json = JSON.parse(responseText);
+				for(let i = 0; i < json.length; i++){
+					let url = 'Data\\Photos\\' + name + '\\' + json[i];
+					let myDiv = document.getElementById("row");
+					let div = document.createElement("div");
+					div.className = "column";
+					let img = document.createElement("img");
+					img.src = url;
+					img.id = json[i];
+					img.alt = json[i];
+					img.onclick = myPics;
+					div.appendChild(img);
+					myDiv.appendChild(div);
+				}
+
+				let myPic = document.getElementById(json[0]);
+				let expandImg = document.getElementById("expandedImg");
+				let imgText = document.getElementById("imgtext");
+				expandImg.src = myPic.src;
+				imgText.innerHTML =  myPic.id.replace(".jpg", '') + ":";
+			}
+
+
+/** Sets up the big picture for when a smaller pic is clicked on the scroller**/
+		function myPics(){
+			let myId = this.id;
+			let myPic = document.getElementById(myId);
+			let expandImg = document.getElementById("expandedImg");
+			let imgText = document.getElementById("imgtext");
+			expandImg.src = myPic.src;
+			imgText.innerHTML =  (myId.replace(".jpg", '') + ":");
+
+
+		}
+
+/** Sets up the member divs to display the different members and positions**/
+		function myMembers(responseText){
+			let mainDiv = document.getElementById("members");
+			let json = JSON.parse(responseText);
+			for(let i = 0; i < json.length; i++){
+				let myDiv = document.createElement("div");
+				let myP = document.createElement("p");
+				let mySpan = document.createElement("span");
+
+				myDiv.className = "myDiv";
+				let myStr = json[i]["name"];
+				myDiv.id =  myStr.replace(/ /g, "_");
+				myDiv.value =json[i]["img"];
+				myDiv.onmouseover = hover;
+				myDiv.onmouseout = out;
+				myP.innerHTML = json[i]["name"];
+				mySpan.innerHTML = json[i]["role"];
+
+
+
+				myDiv.appendChild(myP);
+				myDiv.appendChild(mySpan);
+				mainDiv.appendChild(myDiv);
+			}
+		}
+
+
+/** sets up the new member manes and quotes**/
+		function myNews(responseText) {
+			reset();
+			let mainDiv = document.getElementById("PMems");
+			let json = JSON.parse(responseText);
+			let mems = json["messages"];
+			for(let i = 1; i < mems.length; i++){
+				let span = document.createElement("span");
+				let p = document.createElement("p");
+				span.textContent = (mems[i]["name"]+ ": ");
+				p.textContent = (mems[i]["comment"]);
+				p.insertBefore(span, p.firstChild);
+				mainDiv.appendChild(p);
+			}
+		}
+
+/** changes the member divs background on hover**/
+		function hover(){
+			let myId = this.id;
+			let url = "";
+			if(this.value === "none"){
+				url = "https://static.wixstatic.com/media/9edf81_b80ce19be" +
+				"dbb4557acc606097a1db079.jpg/v1/crop/x_21,y_0,w_219,h_230/fill/" +
+				"w_170,h_179,al_c,q_80,usm_0.66_1.00_0.01/9edf81_b80c" +
+				"e19bedbb4557acc606097a1db079.jpg";
+			}else{
+				url = 'Data/Members/' + myId + '/' + this.value;
+			}
+
+			let myDiv = document.getElementById(myId);
+
+			myDiv.style.backgroundImage = "url("+url +")";
+			myDiv.style.backgroundSize = "300px 330px";
+		}
+
+/** returns the memebr divs to normal when mouse stops hovering**/
+		function out(){
+			let myId =  this.id;
+			let myDiv = document.getElementById(myId);
+			myDiv.style.background = "white";
+		}
+
+		/**clears the display **/
+		function reset(){
+			let myDiv = document.getElementById("PMems");
+			myDiv.innerHTML = "";
+		}
+
+
+
+	/**sends the values the user input into the page as post parameters**/
+	/**to a service and logs the response. **/
+	function send() {
+		let first = document.getElementById("theFirst").value;
+		let last = document.getElementById("theLast").value;
+		let email = document.getElementById("theEmail").value;
+		let why= document.getElementById("theWhy").value;
+		let url = "http://localhost:" +  process.env.PORT + "/";
+		const message = {first: first,
+			last: last,
+			email: email,
+			why: why};
+		const fetchOptions = {
+				method : 'POST',
+				headers : {
+					'Accept': 'application/json',
+					'Content-Type' : 'application/json'
+				},
+				body : JSON.stringify(message)
+		};
+
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				console.log(responseText);
+				callAjax("newMembers", "O");
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+
+
+	}
+
+
+
+
+
+
+		/** returns the response text if the status is in the 200s**/
+		/** otherwise rejects the promise with a message including the status**/
+		function checkStatus(response) {
+			if (response.status >= 200 && response.status < 300) {
+				return response.text();
+			} else if (response.status === 404) {
+				// sends back a different error when we have a 404 than when we have
+				// a different error
+				return Promise.reject(new Error("Sorry, we couldn't find that page"));
+			} else {
+				return Promise.reject(new Error(response.status+": "+response.statusText));
+			}
+		}
+
+}) ();
---
+++ web_server.js
@@ -0,0 +1,160 @@
+/*
+Michael Perez
+CSC 337, Spring 2019
+Final Homework
+
+
+Java script server for the pictures, and the get and send file names and info
+used for Home.html, About.html, ExecutiveBoard.html, Events.html, Member.html
+*/
+
+
+//Desktop\337 Projects\Final Homework
+/* eslint no-implicit-globals: 0 */
+/* global require */
+"use strict";
+const express = require("express");
+const app = express();
+let fs = require('fs');
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+               "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+
+
+
+app.use(express.static('public'));
+
+/** reads data asynchronously fromt he passed in file name **/
+/** returns the contents of the file as a string **/
+function readFile(fileName, name) {
+	let file = '';
+	try {
+		if(name === "none"){
+			file = fs.readdirSync("Data\\" + fileName, 'utf8');
+		}else{
+			file = fs.readdirSync("Data\\" + fileName + "\\" + name, 'utf8');
+    }
+	} catch(e) {
+		console.log('Error:', e.stack);
+	}
+	return file;
+}
+
+/** reads data asynchronously fromt he passed in file name **/
+/** returns the contents of the file as a string **/
+function MyFile(name, mode) {
+  let myFile = '';
+  try {
+		if(mode === "1"){
+			myFile = fs.readFileSync("Data\\Members\\" + name + "\\info.txt", 'utf8');
+		}else{
+			myFile = fs.readFileSync("Data/newMembers.txt", 'utf8');
+		}
+	} catch(e) {
+		console.log('Error:', e.stack);
+	}
+	return myFile;
+}
+
+/** retrievs the name of the pic from the folder**/
+function getImg(name){
+	let myList = readFile("Members", name);
+	let json = myList;
+	for(let i = 0; i < json.length; i++){
+		if(!json[i].endsWith(".txt")){
+			return json[i];
+		}
+	}
+	return "none";
+}
+
+/** sets up the json imputs for the new members **/
+function makeList(myList) {
+	let data = [];
+	let json = myList;
+	for(let i = 0; i < json.length; i++){
+		let myImg = getImg(json[i]);
+		let myLine = MyFile(json[i], "1");
+		let line = myLine.split(":::");
+		let myStr = {"name":line[0], "role":line[1], "link":line[2], "img":myImg};
+		data.push(myStr);
+	}
+	return data;
+}
+
+/** builds a JSON representation of the review data **/
+function buildJson(myLine) {
+  let data = [];
+	let line = myLine.split("&&&&&");
+	console.log(line);
+  for (let i = 0; i < line.length; i++) {
+    let mySingle = line[i].split(":::");
+		let theName  = mySingle[0].replace(/_/g, " ");
+		let myStr = {"name":theName, "comment":mySingle[2]};
+    data.push(myStr);
+	}
+	let myData = {"messages": data};
+	return myData;
+}
+
+
+console.log('web service started');
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+
+
+	let mode = req.query.mode;
+	let name = req.query.name;
+
+  let myfile = "";
+
+	if( mode === "Photos"){
+		myfile = readFile(mode, name);
+	}else if(mode === "Members"){
+    let myList = readFile(mode, name);
+		myfile = makeList(myList);
+  }else if(mode === "newMembers"){
+		let mylines = MyFile("0", "0");
+		myfile = buildJson(mylines);
+	}
+
+
+  // returns an error if one of the parameters is missing
+  if(mode == undefined || name == undefined) {
+    res.status(400);
+    res.send("Missing required parameters");
+  }
+	res.send(JSON.stringify(myfile));
+
+});
+
+
+app.post('/', jsonParser, function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+  let first = req.body.first;
+	let last = req.body.last;
+	let email = req.body.email;
+	let why = req.body.why;
+
+	let myText = "\n&&&&&" + first + "_" + last + ":::" + email + ":::" + why;
+
+	fs.appendFile( "Data/newMembers.txt", myText, function(err) {
+		if(err) {
+			console.log(err);
+			res.status(400);
+		}
+		console.log("The file was saved!");
+		res.send("Success!");
+	});
+});
+
+
+app.listen(process.env.PORT);
